19951a8c87a50488a82961dceb1f2a6bc0d7419f
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-02-28 15:02:45 +0100

    first commit

---
+++ app/Http/Controllers/BoolpressController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Boolpress;
+use Illuminate\Http\Request;
+
+class BoolpressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Boolpress  $boolpress
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Boolpress $boolpress)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Boolpress  $boolpress
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Boolpress $boolpress)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Boolpress  $boolpress
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Boolpress $boolpress)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Boolpress  $boolpress
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Boolpress $boolpress)
+    {
+        //
+    }
+}
---
+++ database/factories/BoolpressFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Boolpress;
+use Faker\Generator as Faker;
+
+$factory->define(Boolpress::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
