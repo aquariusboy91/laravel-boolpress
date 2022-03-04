f7efe6821149cb99773ec51a5b6fa100a87c482a
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-03-04 15:39:12 +0100

    add arraytags

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Model\Tag;
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $arrayTags = [
+            'Animal',
+            'Technology',
+            'Science',
+            'Geography',
+            'Math'
+        ];
+    }
+}
