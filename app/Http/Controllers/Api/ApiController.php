5188aa99a8b7e6d484ac99317e0f2685fe8b6dc5
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-03-08 17:53:34 +0100

    add cards

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Model\Boolpress;
+
 class ApiController extends Controller
 {
     public function index()
--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -3,7 +3,7 @@
         <div class="row row-cols-1 row-cols-md-4 g-4">
             <div class="col" v-for="(item, index) in boolpress" :key="index">
                 <div class="card">
-                    <img :src="'/storage/'+item.image" class="card-img-top" :alt="item.name">
+                    <img :src="'/storage/' + item.image" class="card-img-top" :alt="item.name">
                 <div class="card-body">
                     <h5 class="card-title">{{ item.name }}</h5>
                     <p class="card-text">{{ item.description }}</p>
@@ -24,16 +24,11 @@ import Axios from "axios";
             boolpress: null,
         }
     },
-    created() {
-      this.getBoolpress('http://127.0.0.1:8000/api/boolpress');
-    },
-     methods: {
-          getBoolpress(){
-          Axios.get().then(
+     created(){
+          Axios.get('http://127.0.0.1:8000/api/boolpress').then(
             (result) => {
               this.boolpress = result.data.results.data;
             });
-      }
      }
  }
 
